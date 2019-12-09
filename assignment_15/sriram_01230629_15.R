ff3eb9d1b7a28e2f756fcaa92ef84d16e7b2286d
ssriram2 <55262222+ssriram2@users.noreply.github.com>
2019-12-08 19:17:20 -0500

    Update sriram_01230629_15.R

--- assignment_15/sriram_01230629_15.R
+++ assignment_15/sriram_01230629_15.R
@@ -1,6 +1,8 @@
 library("selectr")
 library("xml2")
 library("rvest")
+library(dplyr)
+library(readr)
 
 url <- "https://nytimes.com"
 webpage <- read_html(url)
@@ -12,12 +14,23 @@ print(without_tags)
 
 # Assignment 15 Task 1: write scripts that extract "titles" and "news summary" of articles out of the scrapped data. 
 # Then, print them out using "print()" statement. 
+Titles <- html_nodes(webpage, "div h2")
+title_tags <- gsub("<.*?>", "", Titles) 
+print(title_tags)
+news_summary <- html_nodes(webpage, "div p")
+newssummary_tags <- gsub("<.*?>", "", news_summary) 
+print(newssummary_tags)
 
 
 # Assignment 15 Task 2: write scripts that oraganize your data as dataframe with column names, "title" and "news summary", respectively.
 # Then, save this dataframe as a CSV file. Name it as "NYT_titles.csv". 
 
-
+max.length <- max(length(title_tags),length(newssummary_tags))
+Titles = c(title_tags, rep(0, max.length - length(title_tags)))
+news_summary = c(newssummary_tags, rep(0, max.length - length(newssummary_tags)))
+nytimes<-data.frame(Titles = Titles , news_summary = news_summary)
+str(nytimes)
+write_csv(nytimes,"NYT_TITLES.csv")
 
 # Assignment 15 Task 3: once you save the CSV file, commit and push it back to your repository (no R scripts involved for Task 3). 
 
